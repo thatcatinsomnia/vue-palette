7d3c2c522ecfffee38634a50fdc413ed372cd2e3
Ruei <ruei@undefined.com>
2021-01-05 23:26:34 +0800

    :tada: init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div class="w-full bg-blue-900 flex-1 flex">
+      <ColorBlock class="bg-red-300">color-1</ColorBlock>
+      <ColorBlock class="bg-blue-300">color-2</ColorBlock>
+      <ColorBlock class="bg-yellow-300">color-3</ColorBlock>
+      <ColorBlock class="bg-green-300">color-4</ColorBlock>
+      <ColorBlock class="bg-purple-300">color-5</ColorBlock>
+    </div>
+
+    <div class="h-56 bg-white flex">
+      <button class="flex-1">lib</button>
+      <button class="flex-1">save</button>
+      <button class="flex-1">generate</button>
+      <button class="flex-1">export</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ColorBlock from './components/ColorBlock.vue';
+</script>
+
+<style></style>
---
+++ src/components/ColorBlock.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-1 text-white text-2xl flex items-center justify-center">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  name: String
+});
+</script>
