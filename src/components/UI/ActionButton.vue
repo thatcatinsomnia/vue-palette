17aab3bd4710e685cb6bfa211e7eebca12f439f8
Ruei <ruei@undefined.com>
2021-01-16 20:27:41 +0800

    :recycle: refactor call to action buttons

---
+++ src/components/UI/ActionButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-24 flex flex-col items-center justify-center">
+    <button class="w-1/2 flex flex-col items-center justify-center">
+      <div class="px-2 py-2 flex justify-center rounded bg-gray-700">
+        <slot name="icon"></slot>
+      </div>
+      <span>
+        <slot name="text"></slot>
+      </span>
+    </button>
+  </div>
+</template>
